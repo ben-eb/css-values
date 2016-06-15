b509c98be06d0cfec0a53852d6ac4a38e8eb2188
Ben Briggs <beneb.info@gmail.com>
2016-06-15 19:24:01 +0100

    Add tests for validator functions.

---
+++ src/__tests__/validators.js
@@ -0,0 +1,38 @@
+import test from 'ava';
+import angle from '../validators/isAngle';
+import integer from '../validators/isInteger';
+import length from '../validators/isLength';
+import number from '../validators/isNumber';
+import percentage from '../validators/isPercentage';
+import time from '../validators/isTime';
+import * as fixtures from '../fixtures';
+
+const validators = {
+    angle,
+    integer,
+    length,
+    number,
+    percentage,
+    time,
+};
+
+function validMacro (t, type, input) {
+    t.truthy(validators[type](input));
+}
+
+function invalidMacro (t, type, input) {
+    t.falsy(validators[type](input));
+}
+
+Object.keys(fixtures).forEach(key => {
+    if (key === '__esModule') {
+        return;
+    }
+    const fixture = fixtures[key];
+    fixture.valid.forEach(value => {
+        test(`${value} (valid ${key})`, validMacro, key, value);
+    });
+    fixture.invalid.forEach(value => {
+        test(`${value} (invalid ${key})`, invalidMacro, key, value);
+    });
+});
