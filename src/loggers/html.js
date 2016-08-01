9dc3230b45cef816887628acdb8beb073109194c
Ben Briggs <beneb.info@gmail.com>
2016-08-01 13:20:35 +0100

    Remove trailing whitespace.

--- src/loggers/html.js
+++ src/loggers/html.js
@@ -35,26 +35,26 @@ export function total () {
                     max-width: 1024px;
                     margin: 20px auto;
                 }
-                
+
                 body, pre code {
                     font-family: monospace, monospace;
                 }
-                
+
                 h1 {
                     font-size: 1rem;
                     font-weight: normal;
                 }
-                
+
                 h1, h1 a {
                     color: #fc3;
                 }
-                
+
                 h1, p {
                     border-bottom: 1px solid #527889;
                     padding: 10px 0;
                     margin: 0;
                 }
-                
+
                 pre {
                     margin-bottom: 40px;
                 }
