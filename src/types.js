8a63561a5997d1699bb1c26d63eaa47f7cb336e6
Ben Briggs <beneb.info@gmail.com>
2016-08-02 22:31:17 +0100

    Add types file for parser.

---
+++ src/types.js
@@ -0,0 +1,13 @@
+export const and = 'and';
+export const data  = 'data';
+export const hash  = 'hash';
+export const keyword = 'keyword';
+export const multiple = 'multiple';
+export const or = 'or';
+export const optional = 'optional';
+export const range = 'range';
+export const space = 'space';
+export const word  = 'word';
+
+export const openSquareBracket = '[';
+export const closeSquareBracket = ']';
