fd642d12ac3ad9ec05604101efadd2bc23433af3
Ivan S <wazza.ivan@gmail.com>
2016-08-06 18:05:54 +0300

    fix: linter issues

--- src/validators/isBlendMode.js
+++ src/validators/isBlendMode.js
@@ -1,22 +1,22 @@
 import isCaseInsensitiveKeyword from './isCaseInsensitiveKeyword';
 
 export const blendValues = [
-	'normal',
-	'multiply',
-	'screen',
-	'overlay',
-	'darken',
-	'lighten',
-	'color-dodge',
-	'color-burn',
-	'hard-light',
-	'soft-light',
-	'difference',
-	'exclusion',
-	'hue',
-	'saturation',
-	'color',
-	'luminosity'
+    'normal',
+    'multiply',
+    'screen',
+    'overlay',
+    'darken',
+    'lighten',
+    'color-dodge',
+    'color-burn',
+    'hard-light',
+    'soft-light',
+    'difference',
+    'exclusion',
+    'hue',
+    'saturation',
+    'color',
+    'luminosity',
 ];
 
 export default node => isCaseInsensitiveKeyword(node, blendValues);
