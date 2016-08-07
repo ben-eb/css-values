1766e3e4e1fd6997af635468063519c095751566
Oleg-docent <oleg.docent2012@gmail.com>
2016-08-08 00:57:12 +0300

    Update and rename isAnimatedFeature.js to isAnimateableFeature.js

--- isAnimateableFeature.js
+++ isAnimateableFeature.js
@@ -1,13 +1,13 @@
 import isCaseInsensitiveKeyword from './isCaseInsensitiveKeyword';
 import isCustomIdent from './isCustomIdent';
 
-export const animatedFeatures = [
+export const animateableFeatures = [
     'scroll-position',
     'contents',
 ];
 
 export default node => {
-    return  isCaseInsensitiveKeyword(node, animatedFeatures) || isCustomIdent(node);
+    return  isCaseInsensitiveKeyword(node, animateableFeatures) || isCustomIdent(node);
 };
 
 export const type = 'node';
