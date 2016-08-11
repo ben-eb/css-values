dca0ada68276df954f373842f5c5cb99f69c1d64
Ben Briggs <beneb.info@gmail.com>
2016-08-11 22:23:28 +0100

    Add node build script.

---
+++ build.js
@@ -0,0 +1,23 @@
+const execa = require('execa');
+const Listr = require('listr');
+
+const run = script => ['run', script];
+
+const tasks = new Listr([{
+    title: 'Transpiling generators',
+    task: () => execa('npm', run('prepublish')),
+}, {
+    title: 'Generating module',
+    task: () => execa('npm', run('generate')),
+}, {
+    title: 'Running tests',
+    task: () => execa('npm', ['test']),
+}, {
+    title: 'Generating coverage report',
+    task: () => execa('npm', run('report')),
+}, {
+    title: 'Updating documentation',
+    task: () => execa('npm', run('markdown')),
+}]);
+
+tasks.run();
