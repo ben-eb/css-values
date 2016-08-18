641a30e37740d78495c6bf0dc0325ec8f70f19e8
Ben Briggs <beneb.info@gmail.com>
2016-08-18 20:29:34 +0100

    Update isCustomIdent validator.

--- src/validators/isCustomIdent.js
+++ src/validators/isCustomIdent.js
@@ -1,3 +1,15 @@
+function isInvalid (value) {
+    return /[^a-z0-9_-]/ig.test(value);
+}
+
+function isCodepoint (value) {
+    return /\\u[a-f0-9]{1,6}/ig.test(value) || /\\[a-f0-9]{1,6}/ig.test(value);
+}
+
+function isValid (value) {
+    return !isInvalid(value) || isCodepoint(value);
+}
+
 export default ({type, value}) => {
     if (type !== 'word') {
         return false;
@@ -9,9 +21,9 @@ export default ({type, value}) => {
         if (value[1] === '-' && value[2] !== '-') {
             return false;
         }
-        return true;
+        return isValid(value);
     }
-    return !/[0-9]/.test(value[0]);
+    return !/[0-9]/.test(value[0]) && isValid(value);
 };
 
 export const type = 'node';
