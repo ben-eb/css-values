f011f107cc26cd2a7836de531342220a72fdf728
Ben Briggs <beneb.info@gmail.com>
2016-08-31 01:03:42 +0100

    Drop unused generators.

--- src/generators/index.js
+++ src/generators/index.js
@@ -1,7 +1,5 @@
-export exportModules from './exportModules';
 export program from './program';
 export property from './property';
 export requireModules from './requireModules';
-export requireNamespacedModules from './requireNamespacedModules';
 export test from './test';
 export validator from './validator';
