f3b718c9bae6d5cb48de776e9d6c728b5b9a28cc
Ben Briggs <beneb.info@gmail.com>
2016-09-07 10:54:18 +0100

    Remove unnecessary exportConst.

--- src/generators/exportConst.js
+++
@@ -1,9 +0,0 @@
-import * as t from 'babel-types';
-import template from '../util/moduleTemplate';
-
-export default ({identifier, exported}) => {
-    return template('export const foo = bar;')({
-        foo: t.identifier(identifier),
-        bar: exported,
-    });
-};
